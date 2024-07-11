53c644921437d8aefc738cc4c5f0a52b97115a69
Mateus Magno <mateusmagnoferreirasoares@gmail.com>
2024-07-11 20:36:13 -0300

    Feat: Guest input and Guest Popup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,32 @@
-import { MapPin, Calendar, ArrowRight } from "lucide-react";
+import {
+  MapPin,
+  Calendar,
+  ArrowRight,
+  UserRoundPlus,
+  Settings2,
+  X,
+} from "lucide-react";
+import { useState } from "react";
 
 export function App() {
+  const [isGuestInputOpen, setIsGuestInputOpen] = useState(false);
+  const [isGuestPopupOpen, setIsGuestPopupOpen] = useState(false);
+
+  function openGuestPopup() {
+    setIsGuestPopupOpen(true);
+  }
+
+  function closeGuestPopup() {
+    setIsGuestPopupOpen(false);
+  }
+
+  function openGuestInput() {
+    setIsGuestInputOpen(true);
+  }
+
+  function closeGuestInput() {
+    setIsGuestInputOpen(false);
+  }
   return (
     <div className="h-screen flex justify-center items-center bg-boxBg bg-no-repeat bg-center">
       <div className="max-w-3xl  w-full px-6 text-center space-y-10">
@@ -10,29 +36,72 @@ export function App() {
             Convide seus amigos e planeje sua próxima viagem!
           </p>
         </div>
+        <div className="space-y-4">
+          <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
+            <div className="space-x-2 flex items-center flex-1">
+              <MapPin className="size-5 text-zinc-400" />
+              <input
+                disabled={isGuestInputOpen}
+                placeholder="para onde você vai?"
+                type="text"
+                className="text-zinc-400 text-lg bg-transparent outline-none flex-1"
+              />
+            </div>
+            <div className="space-x-2 flex items-center">
+              <Calendar className="size-5 text-zinc-400" />
+              <input
+                disabled={isGuestInputOpen}
+                placeholder="Quando?"
+                type="text"
+                className="text-zinc-400 text-lg bg-transparent outline-none w-32"
+              />
+            </div>
+            <div className="h-6 w-px bg-zinc-800" />
+            {isGuestInputOpen ? (
+              <button
+                onClick={closeGuestInput}
+                className="bg-zinc-800 text-zinc-200 rounded-lg px-5 py-2 space-x-2 flex items-center hover:bg-zinc-700"
+              >
+                Alterar local/data
+                <Settings2 />
+              </button>
+            ) : (
+              <button
+                onClick={openGuestInput}
+                className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 space-x-2 flex items-center hover:bg-lime-400"
+              >
+                continuar
+                <ArrowRight />
+              </button>
+            )}
+          </div>
 
-        <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
-          <div className="space-x-2 flex items-center flex-1">
-            <MapPin className="size-5 text-zinc-400" />
-            <input
-              placeholder="para onde você vai?"
-              type="text"
-              className="text-zinc-400 text-lg bg-transparent outline-none flex-1"
-            />
-          </div>
-          <div className="space-x-2 flex items-center">
-            <Calendar className="size-5 text-zinc-400" />
-            <input
-              placeholder="Quando?"
-              type="text"
-              className="text-zinc-400 text-lg bg-transparent outline-none w-32"
-            />
-          </div>
-          <div className="h-6 w-px bg-zinc-800" />
-          <button className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 space-x-2 flex items-center hover:bg-lime-400">
-            continuar
-            <ArrowRight />
-          </button>
+          {isGuestInputOpen && (
+            <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
+              <div className="space-x-2 flex items-center flex-1">
+                <UserRoundPlus className="size-5 text-zinc-400" />
+
+                <button
+                  disabled={isGuestPopupOpen}
+                  onClick={openGuestPopup}
+                  className="text-zinc-400 text-lg bg-transparent outline-none flex-1"
+                >
+                  <span className="flex flex-initial">
+                    Quem estará na viagem?
+                  </span>
+                </button>
+              </div>
+
+              <div className="h-6 w-px bg-zinc-800" />
+              <button
+                disabled={isGuestPopupOpen}
+                className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 space-x-2 flex items-center hover:bg-lime-400"
+              >
+                Confirmar viagem
+                <ArrowRight />
+              </button>
+            </div>
+          )}
         </div>
 
         <p className="text-zinc-500 text-sm">
@@ -48,6 +117,15 @@ export function App() {
           .
         </p>
       </div>
+
+      {isGuestPopupOpen && (
+        <div className=" fixed inset-0 bg-black/60 flex items-center justify-center">
+          <div className="w-[640px] bg-zinc-800 rounded-xl px-6 py-5 shadow-shape flex">
+            <h2 className="flex-1">Selecionar Convidados</h2>
+            <X onClick={closeGuestPopup} className="cursor-pointer" />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
