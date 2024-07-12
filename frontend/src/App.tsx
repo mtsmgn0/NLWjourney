4ada4095d9928e35a7a96b842453d049ecb3a5cb
Mateus Magno <mateusmagnoferreirasoares@gmail.com>
2024-07-11 23:41:52 -0300

    Feat: Popup Guests Style

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,8 @@ import {
   UserRoundPlus,
   Settings2,
   X,
+  AtSign,
+  Plus,
 } from "lucide-react";
 import { useState } from "react";
 
@@ -120,9 +122,79 @@ export function App() {
 
       {isGuestPopupOpen && (
         <div className=" fixed inset-0 bg-black/60 flex items-center justify-center">
-          <div className="w-[640px] bg-zinc-800 rounded-xl px-6 py-5 shadow-shape flex">
-            <h2 className="flex-1">Selecionar Convidados</h2>
-            <X onClick={closeGuestPopup} className="cursor-pointer" />
+          <div className="w-[640px] bg-zinc-900 rounded-xl px-6 py-5 shadow-shape space-y-5">
+            <div className="space-y-2">
+              <div className="flex items-center justify-between w-full">
+                <h2 className="text-lg text-white font-semibold">
+                  Selecionar Convidados
+                </h2>
+                <button type="button" onClick={closeGuestPopup}>
+                  <X className="size-5 text-zinc-400" />
+                </button>
+              </div>
+              <p className="text-zinc-400 text-sm">
+                Os convidados irão receber e-mails para confirmar a participação
+                na viagem.
+              </p>
+            </div>
+            {/* bloco que sera preenchido pelos emails */}
+            <div className="gap-2 flex flex-wrap">
+              {/* email ilustrativo falso */}
+              <div className="bg-zinc-800 py-1.5 px-2.5 rounded-md flex items-center space-x-2.5">
+                <span>exemplo.inventado@gmail.com</span>
+                <button>
+                  <X className="text-zinc-400 size-4" />
+                </button>
+              </div>
+
+              <div className="bg-zinc-800 py-1.5 px-2.5 rounded-md flex items-center space-x-2.5">
+                <span>exemplo.inventado@gmail.com</span>
+                <button>
+                  <X className="text-zinc-400 size-4" />
+                </button>
+              </div>
+
+              <div className="bg-zinc-800 py-1.5 px-2.5 rounded-md flex items-center space-x-2.5">
+                <span>exemplo.inventado@gmail.com</span>
+                <button>
+                  <X className="text-zinc-400 size-4" />
+                </button>
+              </div>
+
+              <div className="bg-zinc-800 py-1.5 px-2.5 rounded-md flex items-center space-x-2.5">
+                <span>exemplo.inventado@gmail.com</span>
+                <button>
+                  <X className="text-zinc-400 size-4" />
+                </button>
+              </div>
+
+              <div className="bg-zinc-800 py-1.5 px-2.5 rounded-md flex items-center space-x-2.5">
+                <span>exemplo.inventado@gmail.com</span>
+                <button>
+                  <X className="text-zinc-400 size-4" />
+                </button>
+              </div>
+
+              <div className="bg-zinc-800 py-1.5 px-2.5 rounded-md flex items-center space-x-2.5">
+                <span>exemplo.inventado@gmail.com</span>
+                <button>
+                  <X className="text-zinc-400 size-4" />
+                </button>
+              </div>
+            </div>{" "}
+            <div className="max-w-xl h-px w-full bg-zinc-800" />
+            <form className="max-w-xl w-full bg-zinc-950 flex items-center rounded-lg px-4 py-2.5 border border-zinc-800 space-x-2.5">
+              <AtSign className="text-zinc-400 size-5" />
+              <input
+                type="text"
+                placeholder="Digite o e-mail do convidado"
+                className="bg-transparent outline-none flex-1"
+              />
+              <button className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 space-x-2 flex items-center hover:bg-lime-400">
+                Convidar
+                <Plus className="size-5" />
+              </button>
+            </form>
           </div>
         </div>
       )}
